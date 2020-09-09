54b51ea06ad0fb4669d8ab178ca5a37cc3e24d11
Canopius <joshuagrey981@gmail.com>
2020-09-09 11:28:29 +0100

    Orginised into folders

---
+++ SummerWork/BranchingSelectionConstantsVariables.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+
+#define A 96 // Constant
+const int b = 7; // Constant
+
+int main() {
+	int c = 100; // Integer var
+	std::string str = "Hello world!"; // String var etc.
+
+	// Selection
+	if (b < c) {
+		std::cout << b << " < " << c << std::endl;
+	} else if (b > c) {
+		std::cout << b << " > " << c << std::endl;
+	} else {
+		std::cout << b << " == " << c << std::endl;
+	};
+
+	for (int i = 0; i < c; i++) {
+		if (i == b) {
+			continue; // Branching
+		}else if(i == 67) {
+			break; // Branching
+		};
+		std::cout << i << std::endl;
+	};
+
+}
