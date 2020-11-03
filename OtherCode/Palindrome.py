4604e9966c06e8c9dfee79ca9cb17d7dc6d460b7
Canopius <joshuagrey981@gmail.com>
2020-11-03 13:15:20 +0000

    Counting vowels and consonants

--- OtherCode/Palindrome.py
+++ OtherCode/Palindrome.py
@@ -1,8 +1,8 @@
-Word = input("Input word: ")
-Word = Word.lower()
-Word.replace(" ", "")
+vowel = ["a","e","i","o","u"]
+cons = ["b","c","d","f","g","g","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+findOccurrences = lambda s, ch : [i for i, letter in enumerate(s) if letter in (b for b in ch)]
+Word = input("Input word: ").lower().replace(" ", "")
+print("There are: {} vowel(s) and {} consonant(s)".format(len(findOccurrences(Word,vowel)), len(findOccurrences(Word,cons))))
 Reverse = Word[::-1]
-if Reverse == Word:
-    print("Yay")
-else:
-    print("Nay")
+x = lambda a, b: a == b
+print(x(Word, Reverse))
