8d20f79a6a4c4f863970b6f958cca8d7e2709c0c
Josh <joshuagrey981@gmail.com>
2021-01-28 13:33:39 +0000

    Storing account details + hashed passwords

--- OtherCode/SweetShopGui/main.py
+++ OtherCode/SweetShopGui/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import uuid
 import hashlib
+import csv
 
 OpenWindow = None
 
@@ -32,7 +33,6 @@ class LoginApplication(Application):
         self.userLabel = self.createLabel("Username", "top")
         self.userInput = self.createEntry("",  "top")
 
-
         self.passwordLabel = self.createLabel("Password", "top")
         self.passwordInput = self.createEntry("",  "top")
         self.passwordInput["show"] = '*'
@@ -40,15 +40,12 @@ class LoginApplication(Application):
         self.login = self.createButton("Login", "top")
         self.login["command"] = self.verifyCreds
 
-
         self.signUp = self.createButton("Sign Up", "top")
         self.signUp["command"] = self.signUpActivated
 
-
     def signUpActivated(self):
-        ShowSignUp(root)
+        ShowSignUp()
         
-
     def verifyCreds(self):
         print(self.userInput.get())
         print(self.passwordInput.get())
@@ -74,20 +71,43 @@ class SignUp(Application):
         self.confirmPasswordInput["show"] = '*'
 
         self.createAccount = self.createButton("Create Account", "top")
+        self.createAccount["command"] = self.storeDetails
 
-        
-def ShowSignUp(oldRoot):
+        self.login = self.createButton("Login", "top")
+        self.login["command"] = self.loginActivated
+
+    def loginActivated(self):
+        ShowLogin()
+
+    def storeDetails(self):
+        if self.passwordInput.get() != self.confirmPasswordInput.get() or len(self.userInput.get()) <= 0 or len(self.passwordInput.get()) <= 0:
+            return False
+
+        hashedPassword = HashPassword(self.passwordInput.get())
+
+        with open('creds.csv', 'a', newline='') as csvfile:
+            credWrite = csv.writer(csvfile, delimiter=',')
+            credWrite.writerow([self.userInput.get()] + [hashedPassword])
+
+
+def ShowSignUp():
     root = tk.Tk()
     app = SignUp(master=root)
     app.mainloop()
 
 def ShowLogin():
-    for index in range(0,len(allWidgets)):
-        allWidgets[index].destroy()
-
     root = tk.Tk()
     app = LoginApplication(master=root)
     app.mainloop()
+
+def HashPassword(password):
+    # uuid is used to generate a random number
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+    
+def CheckPassword(hashedPassword, userPassword):
+    password, salt = hashedPassword.split(':')
+    return password == hashlib.sha256(salt.encode() + userPassword.encode()).hexdigest()
 
 root = tk.Tk()
 app = LoginApplication(master=root)
